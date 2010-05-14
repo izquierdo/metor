604cf0349d2b9f4a83ae839769444b9b3a811e1b
Julio Castillo <juliocc@gmail.com>
2010-05-14 09:34:07 -0430

    Prototipo del wrapper para estaciones davis

---
+++ wrapper_davis.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+import sys
+import csv
+import MySQLdb
+
+estacion_id = 1
+
+# IDs de cada sensor en la BD para cada sensor de esta estacion
+sensor_ids = {
+    "barometer": 1,
+    "pressure": 2,
+    "temperature": 3,
+    "humidity": 4,
+    "wind_speed": 5,
+    "wind_direction":6,
+    "wind_gust": 15,
+    "wind_gust_dir": 16,
+    "rain_rate": 7,
+    "rain": 8,
+    "dewpoint": 9,
+    "windchill": 10,
+    "heat_index": 11,
+    "et": 12,
+    "radiation": 13,
+    "uv": 14
+    }
+
+# en que columnas del csv de entrada se encuentra cada variable
+csv_columns = {
+    "barometer": 3,
+    "pressure": 4,
+    "temperature": 7,
+    "humidity": 9,
+    "wind_speed": 10,
+    "wind_direction": 11,
+    "wind_gust": 12,
+    "wind_gust_dir": 13,
+    "rain_rate": 14,
+    "rain": 15,
+    "dewpoint": 16,
+    "windchill": 17,
+    "heat_index": 18,
+    "et": 19,
+    "radiation": 20,
+    "uv": 21,
+    }
+
+    
+
+def parse_date(s):
+    return datetime.strptime(s[:-6], "%Y-%m-%dT%H:%M:%S")
+
+def process(filename):
+    r = csv.reader(open(filename))
+    r.next()                            # descartar el header
+    dbrows = dict((k, []) for k in sensor_ids)
+    for row in r:
+        assert len(row) == 22
+        date = parse_date(row[0])
+        for name,index in csv_columns.iteritems():
+            try:
+                dbrows[name].append((date, float(row[index])))
+            except ValueError:
+                # print "hubo un error parseando '%s' (%s) como float en el dia %s" % (row[index],
+                #                                                                      name,
+                #                                                                      date)
+                pass
+
+    conn = MySQLdb.connect(host="localhost",
+                           user="emisiones",
+                           passwd="emisiones",
+                           db="emisiones")
+    cur = conn.cursor()
+    for table, values in dbrows.iteritems():
+        cur.executemany("INSERT INTO %s (measureDate, valor, codSensor) VALUES (%%s, %%s, %%s)" % table,
+                        [(date, value, sensor_ids[table]) for date, value in values])
+
+    conn.commit()
+
+if __name__ == "__main__":
+    process(sys.argv[1])
