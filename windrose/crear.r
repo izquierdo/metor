d384e56e03bddb2c7f190eddde79f78350729a12
Julio Castillo <juliocc@gmail.com>
2010-05-28 15:17:10 -0430

    Cambios antiguos que nunca les hice commit

--- windrose/crear.r
+++ windrose/crear.r
@@ -1,14 +1,16 @@
 library(climatol)
+
+crear <- function(){
 datos <- read.csv("archive.csv")
 attach(datos)
-
 windDir[is.na(windDir)] <- 360
-#windDir <- (windDir + (rnorm(length(windDir), 180, 180))) %% 360
-
+windDir <- abs(windDir + runif(length(windDir))*200) %% 360
 b <- seq(0, 360, 22.5)
 h1 <- hist(windDir[0<=windSpeed & windSpeed<2], breaks=b)
 h2 <- hist(windDir[2<=windSpeed & windSpeed<4], breaks=b)
 h3 <- hist(windDir[windSpeed>=4], breaks=b)
 f <- data.frame(rbind(h1$counts, h2$counts, h3$counts), row.names=c('0-2','2-4','>4'))
 rosavent(f, 4, 4)
+detach(datos)
 
+}
